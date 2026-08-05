20ce05887ee6064010e3ab3f52c0bac3942ab0f1
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-08-04 23:20:50 -0300

    directories created

---
+++ scripts/opinion.js
@@ -0,0 +1,30 @@
+import { displayDates } from "./date.mjs";
+displayDates(); 
+
+let counter = 0;
+window.addEventListener('DOMContentLoaded', function() {
+    const label = document.querySelector("strong");
+    const rev = localStorage.getItem("opinions-did"); 
+    if ((parseInt(rev) === 0) || (rev === NaN) || (rev === null)){ 
+        counter++;                          
+        localStorage.setItem("opinions-did", counter);
+        label.textContent = counter;
+    }
+    else {
+        counter = parseInt(rev);
+        counter ++;
+        localStorage.setItem("opinions-did", counter);
+        label.textContent = counter;
+    }
+});
+
+
+
+//Info brom broser tab
+
+const teacherRev = new URLSearchParams(window.location.search);
+
+document.querySelector('#message').innerHTML = `
+    <p>You rated us with ${teacherRev.get('punctuation')} stars!!!</p>
+    <p>We shall improve our service with you review: <p>
+    <p>"${teacherRev.get('wreview')}"</p>`
